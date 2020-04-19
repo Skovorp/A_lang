307c0dade92fa0768623956c36eadaef2234f5f0
Skovorp <46576943+Skovorp@users.noreply.github.com>
2020-04-19 19:30:12 +0300

    сonflicting types

--- machine.c
+++ machine.c
@@ -2,6 +2,7 @@
 
 #include "machine.h"
 #include "stack.h"
+#include "instruction.h"
 
 struct machine *machine_create(void)
 {
@@ -25,16 +26,16 @@ int machine_delete(struct machine *m)
 	return 0;
 }
 
-int machine_push(struct machine *m, int *new_element)
+int machine_push(struct machine *m, struct instruction *Inst)
 {
 	if (!m->stack) {
 		return -1;
 	} else {
-		return (stack_push(m->stack, *new_element));
+		return (stack_push(m->stack, *Inst->operand));
 	}
 }
 
-int machine_pop(struct machine *m, int *fake)
+int machine_pop(struct machine *m, struct instruction *Inst)
 {
 	if (!m->stack) {
 		return -1;
@@ -43,37 +44,42 @@ int machine_pop(struct machine *m, int *fake)
 	}
 }
 
-int machine_top(struct machine *m, int *top_element)
+int machine_top(struct machine *m, struct instruction *Inst)
 {
 	if (!m->stack) {
 		return -1;
 	} else {
-		return (stack_top(m->stack, top_element));
+		return (stack_top(m->stack, Inst->operand));
 	}
 }
 
-int machine_sum(struct machine *m, int *fake)
+int machine_sum(struct machine *m, struct instruction *Inst)
 {
 	int r = 0;
 
 	int a;
 	int b;
-	int *pa;
-	int *pb;
-	pa = &a;
-	pb = &b;
+	struct instruction Inst1;
+	struct instruction Inst2;
+	struct instruction *pInst1;
+	struct instruction *pInst2;
+	pInst1 = &Inst1;
+	pInst2 = &Inst2;
+	pInst1->operand = &a;
+	pInst2->operand = &b;
 
-	r += machine_top(m, pa);
-	r += machine_pop(m, fake);
-	r += machine_top(m, pb);
-	r += machine_pop(m, fake);
+	r += machine_top(pInst1);
+	r += machine_pop(Inst);
+	r += machine_top(pInst2);
+	r += machine_pop(Inst);
 
-	r += machine_push(m, a + b);
+	Inst->operand = *Inst1.operand + *Inst2.operand;
+	r += machine_push(Inst);
 
 	return r;
 }
 
-int machine_mul(struct machine *m, int *fake)
+int machine_mul(struct machine *m, struct instruction *Inst)
 {
 	int r = 0;
 
@@ -94,7 +100,7 @@ int machine_mul(struct machine *m, int *fake)
 	return r;
 }
 
-int machine_sub(struct machine *m, int *fake)
+int machine_sub(struct machine *m, struct instruction *Inst)
 {
 	int r = 0;
 
@@ -115,7 +121,7 @@ int machine_sub(struct machine *m, int *fake)
 	return r;
 }
 
-int machine_div(struct machine *m, int *fake)
+int machine_div(struct machine *m, struct instruction *Inst)
 {
 	int r = 0;
 
--- machine.h
+++ machine.h
@@ -9,14 +9,14 @@ struct machine {
 struct machine *machine_create(void);
 int machine_delete(struct machine *m);
 
-int machine_push(struct machine *m, int *new_element);
-int machine_pop(struct machine *m, int *fake);
-int machine_top(struct machine *m, int *top_element);
+int machine_push(struct machine *m, struct instruction *Inst);
+int machine_pop(struct machine *m, struct instruction *Inst);
+int machine_top(struct machine *m, struct instruction *Inst);
 
-int machine_sum(struct machine *m, int *fake);
-int machine_mul(struct machine *m, int *fake);
-int machine_sub(struct machine *m, int *fake);
-int machine_div(struct machine *m, int *fake);
+int machine_sum(struct machine *m, struct instruction *Inst);
+int machine_mul(struct machine *m, struct instruction *Inst);
+int machine_sub(struct machine *m, struct instruction *Inst);
+int machine_div(struct machine *m, struct instruction *Inst);
 
 
 #endif //A_LANG_MACHINE_H
