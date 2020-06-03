e16d8673d1c74994ec90d0edcabeccf42297eb0b
Andrew Bezzubtsev <xhinhellhx@gmail.com>
2020-06-04 00:23:55 +0300

    [instruction.c]
    + use typedef to make machine_instructions declaration more readable
    ~ improve formatting

--- machine/instruction.c
+++ machine/instruction.c
@@ -12,17 +12,26 @@ struct instruction *instruction_create(void)
 int instruction_delete(struct instruction *inst)
 {
 	free(inst);
-
 	return SUCCESS;
 }
 
+typedef int (*machine_instruction_executor_t)(struct machine *, struct instruction *);
+
 int machine_execute_instruction(struct machine *m, struct instruction *inst)
 {
-	 int (*machine_functions[7])(struct machine*, struct instruction*) = {
-			machine_push, machine_pop, machine_top,
-			machine_sum, machine_sub, machine_mul, machine_div};
-	 if (inst->code >= 7){
-	 	return -1;
-	 }
-	 return (*machine_functions[inst->code])(m, inst);
+	const machine_instruction_executor_t machine_instructions[INSTRUCTIONS_NO] = {
+		machine_push, 
+		machine_pop, 
+		machine_top,
+		machine_sum, 
+		machine_sub, 
+		machine_mul, 
+		machine_div
+	};
+	
+	if (inst->code >= INSTRUCTIONS_NO) {
+		return FAILURE;
+	}
+	
+	return (*machine_functions[inst->code])(m, inst);
 }
